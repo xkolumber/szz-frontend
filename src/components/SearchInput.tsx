3d3e464f4c4eb1a8bbf1e630d040394d9a042f5e
xkolumber <lubosk158@gmail.com>
2024-12-12 18:16:43 +0100

    useDebouncedCallback in search input

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,42 +1,68 @@
-"use client";
+import { useEffect, useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
 import IconZoom from "./Icons/IconZoom";
-// import { usePathname, useRouter, useSearchParams } from "next/navigation";
-// import { useDebouncedCallback } from "use-debounce";
 
 const SearchInput = () => {
-  //   const searchParams = useSearchParams();
-  //   const pathname = usePathname();
-  //   const { replace } = useRouter();
+  const [searchParams, setSearchParams] = useState(() => {
+    const params = new URLSearchParams(window.location.search);
+    return params;
+  });
 
-  //   const handleSearch = useDebouncedCallback((term) => {
-  //     console.log(`Searching... ${term}`);
+  const [query, setQuery] = useState(searchParams.get("query") || "");
 
-  //     const params = new URLSearchParams(searchParams);
-  //     params.set("page", "1");
-  //     if (term) {
-  //       params.set("query", term);
-  //     } else {
-  //       params.delete("query");
-  //     }
-  //     replace(`${pathname}?${params.toString()}`);
-  //   }, 300);
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+
+    const params = new URLSearchParams(window.location.search);
+    params.set("page", "1");
+
+    if (term) {
+      console.log("called");
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+
+    setSearchParams(params);
+    const newUrl = `${window.location.pathname}?${params.toString()}`;
+    window.history.replaceState(null, "", newUrl);
+  }, 300);
+
+  const handleInputChange = (e: any) => {
+    const value = e.target.value;
+    setQuery(value);
+    handleSearch(value);
+  };
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    if (params.get("query") !== query) {
+      setQuery(params.get("query") || "");
+    }
+  }, [searchParams]);
+  // const data = await fetchFilteredPodcasts(query, currentPage);
+
   return (
-    <label className="relative text-gray-400 focus-within:text-gray-600 block ask  md:max-w-none   ">
-      <input
-        type="text"
-        name="email"
-        id="email"
-        placeholder="Vyhľadávanie..."
-        // onChange={(e) => {
-        //   handleSearch(e.target.value);
-        // }}
-        className="form-input !rounded-[48px] border  !border-gray-900 !p-8 !px-4 !bg-white placeholder-gray-400 text-gray-500 appearance-none w-full block !pl-14 focus:outline-none"
-        // defaultValue={searchParams.get("query")?.toString()}
-      />
-      <div className="">
-        <IconZoom />
-      </div>
-    </label>
+    <div className="relative">
+      <label className="search relative text-gray-400 focus-within:text-gray-600 block ask md:max-w-none">
+        <input
+          type="text"
+          placeholder="Vyhľadávanie..."
+          value={query}
+          onChange={handleInputChange}
+          className="form-input !rounded-[48px] border !border-gray-900 !p-8 !px-4 !bg-white placeholder-gray-400 text-gray-500 appearance-none w-full block !pl-14 focus:outline-none"
+        />
+        <div className="">
+          <IconZoom />
+        </div>
+      </label>
+      {query && (
+        <div className="absolute  mt-2 flex flex-col bg-white drop-shadow-md rounded-[16px] w-[300px] z-[3100]">
+          {" "}
+          <p className="">{query}</p>
+        </div>
+      )}
+    </div>
   );
 };
 
