97dac69141821d0717cd4a86ab1db7353870731c
xkolumber <lubosk158@gmail.com>
2024-12-16 16:58:53 +0100

    console log homepage visits

--- src/components/HomePageComponents/HomePageVisitCount.tsx
+++ src/components/HomePageComponents/HomePageVisitCount.tsx
@@ -4,14 +4,19 @@ import { useEffect, useState } from "react";
 const VisitCounter = () => {
   const [visitCount, setVisitCount] = useState(null);
 
-  const key = "visit";
+  const key = "visits";
+
+  const namespace =
+    import.meta.env.DEPLOYED_URL || "my-unique-namespace12385455564uyhkjk";
 
   useEffect(() => {
     const incrementVisitCount = async () => {
+      console.log("hello?");
       try {
         const response = await axios.get(
-          `https://api.countapi.xyz/hit/${import.meta.env.DEPLOYED_URL}/${key}`
+          `https://api.countapi.xyz/hit/${namespace}/${key}`
         );
+        console.log(response);
         setVisitCount(response.data.value);
       } catch (error) {
         console.error("Error fetching visit count:", error);
